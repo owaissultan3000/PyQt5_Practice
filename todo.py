0b279129fd38a0fb0a20be257faa05b284f90b28
Owais Sultan <59525075+owaissultan3000@users.noreply.github.com>
2022-07-24 14:41:45 +0500

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,150 +1,95 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QStackedLayout,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit, QListView, QFormLayout, QListWidget, QMenuBar
+)
 
 
-class Ui_TodoApplication(object):
-    def setupUi(self, TodoApplication):
-        TodoApplication.setObjectName("TodoApplication")
-        TodoApplication.resize(1000, 1000)
-        TodoApplication.setAutoFillBackground(False)
-        self.label = QtWidgets.QLabel(TodoApplication)
-        self.label.setGeometry(QtCore.QRect(350, 60, 271, 61))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(22)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.Add_Task_Button = QtWidgets.QPushButton(TodoApplication,clicked=lambda: self.add_task())
-        self.Add_Task_Button.setGeometry(QtCore.QRect(750, 190, 200, 50))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.Add_Task_Button.setFont(font)
-        self.Add_Task_Button.setCheckable(False)
-        self.Add_Task_Button.setChecked(False)
-        self.Add_Task_Button.setObjectName("Add_Task_Button")
-        self.Day_Label = QtWidgets.QLabel(TodoApplication)
-        self.Day_Label.setGeometry(QtCore.QRect(80, 250, 171, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.Day_Label.setFont(font)
-        self.Day_Label.setObjectName("Day_Label")
-        self.Task_Input = QtWidgets.QLineEdit(TodoApplication)
-        self.Task_Input.setGeometry(QtCore.QRect(270, 200, 411, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.Task_Input.setFont(font)
-        self.Task_Input.setObjectName("Task_Input")
-        self.Task_Label = QtWidgets.QLabel(TodoApplication)
-        self.Task_Label.setGeometry(QtCore.QRect(80, 200, 171, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        self.Task_Label.setFont(font)
-        self.Task_Label.setObjectName("Task_Label")
-        self.Day_Input = QtWidgets.QLineEdit(TodoApplication)
-        self.Day_Input.setGeometry(QtCore.QRect(270, 250, 411, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.Day_Input.setFont(font)
-        self.Day_Input.setObjectName("Day_Input")
-        self.Delete_Task_Button = QtWidgets.QPushButton(TodoApplication,clicked=lambda: self.delete_task())
-        self.Delete_Task_Button.setGeometry(QtCore.QRect(745, 550, 251, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        font.setBold(True)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.Delete_Task_Button.setFont(font)
-        self.Delete_Task_Button.setCheckable(False)
-        self.Delete_Task_Button.setChecked(False)
-        self.Delete_Task_Button.setObjectName("Delete_Task_Button")
-        self.Clear_Button = QtWidgets.QPushButton(TodoApplication,clicked=lambda: self.clear_task())
-        self.Clear_Button.setGeometry(QtCore.QRect(750, 250, 200, 50))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.Clear_Button.setFont(font)
-        self.Clear_Button.setCheckable(False)
-        self.Clear_Button.setChecked(False)
-        self.Clear_Button.setObjectName("Clear_Button")
-        self.Complete_Task_Button = QtWidgets.QPushButton(TodoApplication,clicked=lambda: self.complete_task())
-        self.Complete_Task_Button.setGeometry(QtCore.QRect(745, 700, 251, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        font.setBold(True)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.Complete_Task_Button.setFont(font)
-        self.Complete_Task_Button.setCheckable(False)
-        self.Complete_Task_Button.setChecked(False)
-        self.Complete_Task_Button.setObjectName("Complete_Task_Button")
-        self.listWidget = QtWidgets.QListWidget(TodoApplication)
-        self.listWidget.setGeometry(QtCore.QRect(130, 420, 601, 501))
-        self.listWidget.setObjectName("listWidget")
-        self.label_2 = QtWidgets.QLabel(TodoApplication)
-        self.label_2.setGeometry(QtCore.QRect(370, 370, 161, 41))
-        font = QtGui.QFont()
-        font.setPointSize(15)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-        self.retranslateUi(TodoApplication)
-        QtCore.QMetaObject.connectSlotsByName(TodoApplication)
+        self.setGeometry(800, 200, 1000, 1000)
+        self.setWindowTitle("Todo Application")
+        header_label = QtWidgets.QLabel("TODO APP", alignment=Qt.AlignHCenter)
+        header_label.move(700, 90)
+        header_label.setFont(QFont('Arial', 18, QFont.Bold))
 
-    def retranslateUi(self, TodoApplication):
-        _translate = QtCore.QCoreApplication.translate
-        TodoApplication.setWindowTitle(_translate("TodoApplication", "Form"))
-        self.label.setText(_translate("TodoApplication", "TODO APP"))
-        self.Add_Task_Button.setText(_translate("TodoApplication", "Add Task"))
-        self.Day_Label.setText(_translate("TodoApplication", "Day & Time"))
-        self.Task_Input.setPlaceholderText(_translate("TodoApplication", "Add Task"))
-        self.Task_Label.setText(_translate("TodoApplication", "Add Task"))
-        self.Day_Input.setPlaceholderText(_translate("TodoApplication", "Add Day & Time"))
-        self.Delete_Task_Button.setText(_translate("TodoApplication", "Delete Task"))
-        self.Clear_Button.setText(_translate("TodoApplication", "Clear"))
-        self.Complete_Task_Button.setText(_translate("TodoApplication", "Complete Task"))
-        self.label_2.setText(_translate("TodoApplication", "All Tasks"))
+        task_label = QtWidgets.QLabel("Task")
+        task_label.setFont(QFont('Arial', 12, QFont.Bold))
+        self.task_input = QLineEdit()
+        self.task_input.setPlaceholderText("Add Task")
+        self.task_input.setFont(QFont('Arial', 12))
+        task_label.move(300, 100)
+
+        day_label = QtWidgets.QLabel("Day & Time")
+        day_label.setFont(QFont('Arial', 12, QFont.Bold))
+        self.day_input = QLineEdit()
+        self.day_input.setPlaceholderText("Add Day & Time")
+        self.day_input.setFont(QFont('Arial', 12))
+
+        add_task = QtWidgets.QPushButton("Add Task", clicked=lambda: self.add_task())
+        add_task.setFont(QFont('Arial', 12, QFont.Bold))
+
+        clear_task = QtWidgets.QPushButton("Clear", clicked=lambda: self.clear_task())
+        clear_task.setFont(QFont('Arial', 12, QFont.Bold))
+
+        self.all_task = QListWidget()
+        self.all_task.setFont((QFont('Arial', 12, QFont.Bold)))
+        delete_task = QtWidgets.QPushButton("Delete Task", clicked=lambda: self.delete_task())
+        delete_task.setFont(QFont('Arial', 12, QFont.Bold))
+
+        complete_task = QtWidgets.QPushButton("Complete Task", clicked=lambda: self.complete_task())
+        complete_task.setFont(QFont('Arial', 12, QFont.Bold))
+
+        form_layout = QFormLayout()
+        self.setLayout(form_layout)
+
+        form_layout.addRow(header_label)
+        form_layout.addRow(task_label, self.task_input)
+        form_layout.addRow(day_label, self.day_input)
+        form_layout.addRow(add_task)
+        form_layout.addRow(clear_task)
+        form_layout.addRow(self.all_task)
+        form_layout.addRow(complete_task)
+        form_layout.addRow(delete_task)
+        self.show()
 
     def add_task(self):
-        # print(self.Task_Input.text())
-        # print(self.Day_Input.text())
-        task = self.Task_Input.text()
-        day_time = self.Day_Input.text()
+        task = self.task_input.text()
+        day_time = self.day_input.text()
         data = task + " ( " + day_time + " )"
-        self.listWidget.addItem(data)
-        self.Task_Input.clear()
-        self.Day_Input.clear()
+        self.all_task.addItem(data)
+        self.task_input.clear()
+        self.day_input.clear()
         data = ""
 
     def clear_task(self):
-        self.Task_Input.clear()
-        self.Day_Input.clear()
+        self.task_input.clear()
+        self.day_input.clear()
 
     def delete_task(self):
-        selected = self.listWidget.currentRow()
-        self.listWidget.takeItem(selected)
+        selected = self.all_task.currentRow()
+        self.all_task.takeItem(selected)
 
     def complete_task(self):
-        selected = self.listWidget.currentRow()
-        self.listWidget.takeItem(selected)
+        selected = self.all_task.currentRow()
+        self.all_task.takeItem(selected)
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    TodoApplication = QtWidgets.QWidget()
-    ui = Ui_TodoApplication()
-    ui.setupUi(TodoApplication)
-    TodoApplication.show()
-    sys.exit(app.exec_())
+app = QApplication([])
+
+window = MainWindow()
+
+app.exec()
